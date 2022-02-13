5827fa0802bdf72051bb2ca34630fb5364a0bf6f
友情小爪 <843213558@qq.com>
2022-02-13 23:20:30 +0800

    support reg task

--- mmcls/models/losses/__init__.py
+++ mmcls/models/losses/__init__.py
@@ -6,6 +6,7 @@ from .cross_entropy_loss import (CrossEntropyLoss, binary_cross_entropy,
 from .focal_loss import FocalLoss, sigmoid_focal_loss
 from .label_smooth_loss import LabelSmoothLoss
 from .seesaw_loss import SeesawLoss
+from .reg_loss import MSELoss
 from .utils import (convert_to_one_hot, reduce_loss, weight_reduce_loss,
                     weighted_loss)
 
@@ -13,5 +14,5 @@ __all__ = [
     'accuracy', 'Accuracy', 'asymmetric_loss', 'AsymmetricLoss',
     'cross_entropy', 'binary_cross_entropy', 'CrossEntropyLoss', 'reduce_loss',
     'weight_reduce_loss', 'LabelSmoothLoss', 'weighted_loss', 'FocalLoss',
-    'sigmoid_focal_loss', 'convert_to_one_hot', 'SeesawLoss'
+    'sigmoid_focal_loss', 'convert_to_one_hot', 'SeesawLoss', 'MSELoss',
 ]
