28c624e87b83478b95f8f13e4c554a33ac37bcdd
友情小爪 <843213558@qq.com>
2022-02-22 11:35:30 +0800

    add class_accuracy

--- mmcls/core/evaluation/__init__.py
+++ mmcls/core/evaluation/__init__.py
@@ -1,12 +1,12 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .eval_hooks import MyDistEvalHook, MyEvalHook
 from .eval_metrics import (calculate_confusion_matrix, f1_score, precision,
-                           precision_recall_f1, recall, support)
+                           precision_recall_f1, recall, support, class_accuracy)
 from .mean_ap import average_precision, mAP
 from .multilabel_eval_metrics import average_performance
 
 __all__ = [
     'MyDistEvalHook', 'MyEvalHook', 'precision', 'recall', 'f1_score', 'support',
     'average_precision', 'mAP', 'average_performance',
-    'calculate_confusion_matrix', 'precision_recall_f1'
+    'calculate_confusion_matrix', 'precision_recall_f1', 'class_accuracy',
 ]
